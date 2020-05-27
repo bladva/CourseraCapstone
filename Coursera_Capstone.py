6ba1e6472d5bfbf3d40a46dcfc168a923cc920c9
bladva <56904517+bladva@users.noreply.github.com>
2020-05-27 17:42:02 +0100

    Create Coursera_Capstone.py

---
+++ Coursera_Capstone.py
@@ -0,0 +1,17 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# This Notebook will be used for the Coursera Capstone Project
+
+# %%
+import numpy as np
+import pandas as pd
+
+
+# %%
+print ("Hello Capstone Project Course!")
+
+
+# %%
+
+
